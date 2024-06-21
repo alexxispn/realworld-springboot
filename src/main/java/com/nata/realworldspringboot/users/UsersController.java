a56c9585e52c51a71f422db777eb6b4ee4134886
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2024-06-21 18:06:01 +0100

    fix: make app compile

---
+++ src/main/java/com/nata/realworldspringboot/users/UsersController.java
@@ -0,0 +1,21 @@
+package com.nata.realworldspringboot.users;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @PostMapping
+    public ResponseEntity<Object> register(@RequestBody UserRequest userRequest) {
+        System.out.println("Por aqui ent´ñri ");
+        return ResponseEntity.created(null).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> get() {
+        return ResponseEntity.ok().build();
+    }
+
+}
